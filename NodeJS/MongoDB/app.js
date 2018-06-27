5af1f7c15659760a7d1615d1c0ca1bc973288bf0
MinYoung Lee <avadaisuki68@gmail.com>
2018-06-27 13:53:52 +0900

    app.js

--- NodeJS/MongoDB/app.js
+++ NodeJS/MongoDB/app.js
@@ -1,3 +1,60 @@
+var indexCollection = function(db, callback){
+	db.collection('documents').createIndex({ "a": 1 }, function(err, results){
+		console.log(results);
+		callback();
+	});
+}
+
+
+
+var removeDocument = function(db, callback){
+	var collection = db.collection('documents');
+	collection.deleteOne({a:3}, function(err, result){
+		assert.equal(err, null);
+		assert.equal(1, result.result.n);
+		console.log("Removed the document with the field equal to 3");
+		callback(result);
+	});
+}
+
+
+
+var updateDocument = function(db, callback){
+	var collection = db.collection('documents');
+	collection.updateOne({a:2}, {$set: {b:1}}, function(err, result){
+		assert.equal(err, null);
+		assert.equal(1, result.result.n);
+		console.log("Updated the document with the field equal to 2");
+		callback(result);
+	});
+}
+
+
+
+var findDocumentsQuerry = function(db, callback){
+	var collection = db.collection('documents');
+	collection.find({'a':3}).toArray(function(err, docs){
+		assert.equal(err, null);
+		console.log("Found the following documents");
+		console.log(docs);
+		callback(docs);
+	});
+}
+
+
+
+var findDocuments = function(db, callback){
+	var collection = db.collection('documents');	  // 'documents' collection으로 연결
+	collection.find({}).toArray(function(err, docs){  // 'documents'안의 데이터들을 Array로 바꿔서 docs안에 넣는다.
+		assert.equal(err, null);
+		console.log("Found the following documents");
+		console.log(docs);
+		callback(docs);
+	});
+}
+
+
+
 var insertDocuments = function(db, callback){		// function expression.
 	var collection = db.collection('documents');	// open 'documents'collection
         collection.insertMany([				
@@ -23,7 +80,24 @@ MongoClient.connect(url, function(err, db){		// 연결을 했을 때에 만일 m
 	assert.equal(null, err);
 	console.log("connected successfully to server");
 
-	insertDocuments(db, function() {		//위의 함수를 사용한다.
+	/*insertDocuments(db, function() {		//위의 함수를 사용한다.
+		updateDocument(db, function(){
+			findDocuments(db, function(){
+				db.close();
+			});
+		});
+	});*/
+
+	/*updateDocument(db, function(){
+		removeDocument(db, function(){
+			findDocuments(db, function(){
+				db.close();
+			});
+		});
+	});*/
+
+	indexCollection(db, function() {
 		db.close();
 	});
+
 });
