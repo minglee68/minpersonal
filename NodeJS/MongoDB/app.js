77dbb75b7754ead848c28adb2efe510e09809ab6
MinYoung Lee <avadaisuki68@gmail.com>
2018-06-27 10:28:08 +0900

    app.js

--- NodeJS/MongoDB/app.js
+++ NodeJS/MongoDB/app.js
@@ -7,22 +7,23 @@ var insertDocuments = function(db, callback){		// function expression.
                 assert.equal(3, result.result.n);	
                 assert.equal(3, result.ops.length);
                 console.log("Inserted 3 documents into the collection");
+		// 여기까지가 callback function이 일어나기 전의 내용들.
                 callback(result);
         });
 }
 
 
 
-var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient;	// MongoDB Module을 부른다.
 var assert = require('assert');				// Assert module checks if statement in it is true or not.
 
-var url = 'mongodb://localhost:27017/myproject';
-
-MongoClient.connect(url, function(err, db){
+var url = 'mongodb://localhost:27017/myproject';	// localhost라는 host로 27017을 통해서 연결하고
+							// mongodb의 myproject DB로 연결한다.
+MongoClient.connect(url, function(err, db){		// 연결을 했을 때에 만일 myproject라는 DB가 없으면 새로 만든다.
 	assert.equal(null, err);
 	console.log("connected successfully to server");
 
-	insertDocuments(db, function() {
+	insertDocuments(db, function() {		//위의 함수를 사용한다.
 		db.close();
 	});
 });
